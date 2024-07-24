3e97b580a0ce9a92551e615eb9b514a900dfad5a
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-24 11:50:32 +0200

    add file for delete tasks and fix views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,13 +5,47 @@ from catalog.views import (
     TaskListView,
     TaskCreateView,
     home_redirect,
+    toggle_complete_todo,
+    TaskDeleteView,
+    TaskUpdateView,
 )
 
 urlpatterns = [
-    path("", home_redirect, name="home"),
-    path("tasks/", TaskListView.as_view(), name="task-list"),
-    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
-    path("tags/", TagListView.as_view(), name="tag-list"),
+    path(
+        "",
+        home_redirect,
+        name="home"
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list"
+    ),
+    path(
+        "tasks/create",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list"
+    ),
+    path(
+        "tasks/<int:pk>/toggle-complete/",
+        toggle_complete_todo,
+        name="toggle-complete"
+    ),
 ]
 
 app_name = "catalog"
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect, HttpRequest
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views import generic
 
@@ -33,6 +34,29 @@ class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("catalog:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("catalog:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("catalog:task-list")
+
+
+def toggle_complete_todo(
+        request: HttpRequest,
+        pk: int
+) -> HttpResponseRedirect:
+    task = get_object_or_404(Task, pk=pk)
+
+    task.is_done = not task.is_done
+    task.save()
+
+    return HttpResponseRedirect(reverse("catalog:task-list"))
 
 
 class TagListView(generic.ListView):

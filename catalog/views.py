ecec4fba9f241a0b3d94d8c903f34d87a3ce99da
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-24 10:15:04 +0200

    fix files index.html and add file base.html

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,5 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-@login_required
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "catalog/index.html")
